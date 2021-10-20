e5b19cbd92f7f4516cdbd9b4c25a9f9fb05c62e7
Marx <marxborgesmachado@gmail.com>
2021-10-20 13:45:19 -0300

    End screen game

--- Game.py
+++ Game.py
@@ -1,11 +1,11 @@
 import pygame
 
-player = pygame.image.load("images/Player.png")
+player = pygame.image.load("images/Player(ed).png")
 pistol = pygame.image.load("images/Pistol.png")
-target = pygame.image.load("images/Target.png")
-tubeDown = pygame.image.load("images/Tube(down).png")
-tubeUp = pygame.image.load("images/Tube(up).png")
-door = pygame.image.load("images/Door.png")
+target = pygame.image.load("images/Target(ed).png")
+tubeDown = pygame.image.load("images/Tube(down)(ed).png")
+tubeUp = pygame.image.load("images/Tube(up)(ed).png")
+door = pygame.image.load("images/Door(ed).png")
 
 white = (255, 255, 255)
 
@@ -14,4 +14,6 @@ def StartGame(screen):
     screen.fill(white)
     screen.blit(player, (0, 250))
     screen.blit(tubeDown, (310, 350))
-    screen.blit(tubeUp, (280, -450))
+    screen.blit(tubeUp, (310, -350))
+    screen.blit(door, (310, 215))
+    screen.blit(target, (290, 30))
